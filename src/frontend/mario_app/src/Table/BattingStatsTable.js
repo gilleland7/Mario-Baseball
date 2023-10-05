407206006d2f13bac2cf188b9209c02c9395ea73
gilleland7 <49695100+gilleland7@users.noreply.github.com>
2023-10-05 19:57:30 -0400

    CSS fixes

--- src/frontend/mario_app/src/Table/BattingStatsTable.js
+++ src/frontend/mario_app/src/Table/BattingStatsTable.js
@@ -3,64 +3,64 @@ import './StatsTable.css';
 // Example of a data array that
 // you might receive from an API
 const data = [
-    { Player: "Luigi", PA: 23, AB: 50, '3B': 1, WAR: 10.1},
-    { Player: "Mario", PA: 3, AB: 1, '2B': 1, SLG: '.156%', WAR: 11},
-    { Player: "Bowser", PA: 3, AB: 1, '2B': 1, SLG: '.156%', WAR: 11},
-    { Player: "Peach", PA: 3, AB: 1, '2B': 1, SLG: '.156%', WAR: 11},
-    { Player: "Birdo", PA: 3, AB: 1, '2B': 1, SLG: '.156%', WAR: 11},
-    { Player: "Yoshi", PA: 3, AB: 1, '2B': 1, SLG: '.156%', WAR: 11},
-    { Player: "Wario", PA: 3, AB: 1, '2B': 1, SLG: '.156%', WAR: 11},
-    { Player: "Diddy Kong", PA: 3, AB: 1, '2B': 1, SLG: '.156%', WAR: 11},
-    { Player: "King K. Ruel", PA: 3, AB: 1, '2B': 1, SLG: '.156%', WAR: 11},
+    { Player: "Luigi", PA: 23, AB: 50, H:1, '2B':2, '3B': 1, HR:5, RBI:10, SH:0, SO:0, BB:1, HBP:0, BA:.256, OBP:1.000, SLG: 0.000, OPS: 0.123, SB:10, CS:1, WAR: 10.1},
+    { Player: "Mario", PA: 3, AB: 1, '2B': 1, SLG: '.156', WAR: 11},
+    { Player: "Bowser", PA: 3, AB: 1, '2B': 1, SLG: '.156', WAR: 11},
+    { Player: "Peach", PA: 3, AB: 1, '2B': 1, SLG: '.156', WAR: 11},
+    { Player: "Birdo", PA: 3, AB: 1, '2B': 1, SLG: '.156', WAR: 11},
+    { Player: "Yoshi", PA: 3, AB: 1, '2B': 1, SLG: '.156', WAR: 11},
+    { Player: "Wario", PA: 3, AB: 1, '2B': 1, SLG: '.156', WAR: 11},
+    { Player: "Diddy Kong", PA: 3, AB: 1, '2B': 1, SLG: '.156', WAR: 11},
+    { Player: "King K. Ruel", PA: 3, AB: 1, '2B': 1, SLG: '.156', WAR: 11},
 ]
 
 function BattingStatsTable() {
     return (
-        <div className="StatsTable">
-            <table>
-                <tr>
-                    <th>Player</th>
-                    <th>PA</th>
-                    <th>AB</th>
-                    <th>H</th>
-                    <th>2B</th>
-                    <th>3B</th>
-                    <th>HR</th>
-                    <th>RBI</th>
-                    <th>SH</th>
-                    <th>SO</th>
-                    <th>BB</th>
-                    <th>HBP</th>
-                    <th>BA</th>
-                    <th>OBP</th>
-                    <th>SLG</th>
-                    <th>OPS</th>
-                    <th>SB</th>
-                    <th>CS</th>
-                    <th>WAR</th>
+        <div class="StatsTableClass">
+            <table class="StatsTable">
+                <tr class="StatsRow">
+                    <th class="StatsHeader">Player</th>
+                    <th class="StatsHeader">PA</th>
+                    <th class="StatsHeader">AB</th>
+                    <th class="StatsHeader">H</th>
+                    <th class="StatsHeader">2B</th>
+                    <th class="StatsHeader">3B</th>
+                    <th class="StatsHeader">HR</th>
+                    <th class="StatsHeader">RBI</th>
+                    <th class="StatsHeader">SH</th>
+                    <th class="StatsHeader">SO</th>
+                    <th class="StatsHeader">BB</th>
+                    <th class="StatsHeader">HBP</th>
+                    <th class="StatsHeader">BA</th>
+                    <th class="StatsHeader">OBP</th>
+                    <th class="StatsHeader">SLG</th>
+                    <th class="StatsHeader">OPS</th>
+                    <th class="StatsHeader">SB</th>
+                    <th class="StatsHeader">CS</th>
+                    <th class="StatsHeader">WAR</th>
                 </tr>
                 {data.map((val, key) => {
                     return (
-                        <tr key={key} className="rt-tr-group">
-                            <td>{val.Player}</td>
-                            <td>{val.PA}</td>
-                            <td>{val.AB}</td>
-                            <td>{val.H}</td>
-                            <td>{val['2B']}</td>
-                            <td>{val['3B']}</td>
-                            <td>{val.HR}</td>
-                            <td>{val.RBI}</td>
-                            <td>{val.SH}</td>
-                            <td>{val.SO}</td>
-                            <td>{val.BB}</td>
-                            <td>{val.HBP}</td>
-                            <td>{val.BA}</td>
-                            <td>{val.OBP}</td>
-                            <td>{val.SLG}</td>
-                            <td>{val.OPS}</td>
-                            <td>{val.SB}</td>
-                            <td>{val.CS}</td>
-                            <td>{val.WAR}</td>
+                        <tr key={key} class="StatsRow rt-tr-group">
+                            <td class="StatsData">{val.Player}</td>
+                            <td class="StatsData">{val.PA}</td>
+                            <td class="StatsData">{val.AB}</td>
+                            <td class="StatsData">{val.H}</td>
+                            <td class="StatsData">{val['2B']}</td>
+                            <td class="StatsData">{val['3B']}</td>
+                            <td class="StatsData">{val.HR}</td>
+                            <td class="StatsData">{val.RBI}</td>
+                            <td class="StatsData">{val.SH}</td>
+                            <td class="StatsData">{val.SO}</td>
+                            <td class="StatsData">{val.BB}</td>
+                            <td class="StatsData">{val.HBP}</td>
+                            <td class="StatsData">{val.BA}</td>
+                            <td class="StatsData">{val.OBP}</td>
+                            <td class="StatsData">{val.SLG}</td>
+                            <td class="StatsData">{val.OPS}</td>
+                            <td class="StatsData">{val.SB}</td>
+                            <td class="StatsData">{val.CS}</td>
+                            <td class="StatsData">{val.WAR}</td>
                         </tr>
                     )
                 })}
--- src/frontend/mario_app/src/Table/StandingsTable.js
+++ src/frontend/mario_app/src/Table/StandingsTable.js
@@ -5,29 +5,29 @@ import './StandingsTable.css';
 const data = [
     { Team: "Luigi Knights", W: 6, L: 0, GB: "-"},
     { Team: "Yoshi Eggs", W: 5, L: 1, GB: 1},
-    { Team: "Mario Fireballs", W: 2, L: 4, GB: 4},
-    { Team: "Peach Monarchs", W: 1, L: 5, GB: 5},
+    { Team: "Waluigi Spitballs", W: 2, L: 4, GB: 4},
+    { Team: "Bowser Jr. Rookies", W: 111, L: 51, GB: 15},
 ]
 
 const division = "Mushroom";
 
 function BattingStatsTable() {
     return (
-        <div className="StatsTable">
-            <table>
-                <tr>
-                    <th>{division}</th>
-                    <th>W</th>
-                    <th>L</th>
-                    <th>GB</th>                
+        <div class="StandingsTableClass">
+            <table class="StandingsTable">
+                <tr class="StandingsRow">
+                    <th class="StandingsHeader">{division}</th>
+                    <th class="StandingsHeader">W</th>
+                    <th class="StandingsHeader">L</th>
+                    <th class="StandingsHeader">GB</th>                
                 </tr>
                 {data.map((val, key) => {
                     return (
-                        <tr key={key} className="rt-tr-group">
-                            <td>{val.Team}</td>
-                            <td>{val.W}</td>
-                            <td>{val.L}</td>
-                            <td>{val.GB}</td>
+                        <tr key={key} class="StandingsRow rt-tr-group">
+                            <td class="StandingsData">{val.Team}</td>
+                            <td class="StandingsData">{val.W}</td>
+                            <td class="StandingsData">{val.L}</td>
+                            <td class="StandingsData">{val.GB}</td>
                         </tr>
                     )
                 })}
